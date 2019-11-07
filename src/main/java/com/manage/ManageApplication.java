c1299f8edec8056f5ed03f6d5fc2007ca95fe5ec
xuyanqi <411990453@qq.com>
2019-11-07 13:29:36 +0800

    连接数据库提交

--- src/main/java/com/manage/ManageApplication.java
+++ src/main/java/com/manage/ManageApplication.java
@@ -1,9 +1,11 @@
 package com.manage;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.manage.module.mapper")
 public class ManageApplication {
 
 	public static void main(String[] args) {
